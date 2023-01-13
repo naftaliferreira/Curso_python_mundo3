efdc6d84c941681fcc7eeed047a939df4799d1f2
Naftali Ferreira <naftalibarbosaferreira@gmail.com>
2023-01-13 12:26:21 -0300

    update exercícios

--- 2.Listas em Python/bExercício84.py
+++ 2.Listas em Python/bExercício84.py
@@ -13,14 +13,22 @@ dados_indiv = list()
 
 # contagem de usuários cadastrados
 tot = 0
+maior = menor = 0
 
 # entrada de dados em loop
 while True:
     dados_indiv.append(str(input('Digite o nome: ')))
-    dados_indiv.append(int(input('Digite a idade: ')))
-    
+    dados_indiv.append(int(input('Digite o seu peso: ')))
+# filtro de maior e menor peso
+    if len(dados_indiv) > maior:
+        maior = dados_indiv[1]
+    if dados_indiv[1] > maior:
+        maior = dados_indiv[1]
+    if dados_indiv[1] < menor:
+        menor = dados_indiv[1]
+
 # Copiando os dados da lista individual para a lista grupo
-    dados_grupo.append(dados_indiv[:])
+    dados_grupo.append(dados_indiv[:])   
     dados_indiv.clear()
     tot += 1
    
@@ -35,9 +43,9 @@ while True:
 print(f'Foram cadastradas um total de {tot} pessoas.')
 
 # maior peso
-
+print(f'O maior peso cadastrado é {maior}kg')
 
 # menor peso
-
+print(f'O menor peso cadastrado é {menor}kg')
 
 
