60c0890d416dc41b2b65c759b0210a0703b6ef88
Naftali Ferreira <naftalibarbosaferreira@gmail.com>
2023-01-16 12:23:54 -0300

    update exercícios

--- 2.Listas em Python/bExercício87.py
+++ 2.Listas em Python/bExercício87.py
@@ -1,5 +1,26 @@
-""" Faça um programa que ajude um jogador da MEGA-SENA a criar palpites.
-O programa vai perguntar quantos jogos serão gerados e vai sortear 6
-numeros entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta"""
+""" Aprimore o desafio anterior, mostrando no final:  
+
+  A) A soma de todos os valores pares digitados.  
+
+  B) A soma dos valores da terceira coluna. 
+
+ C) O maior valor da segunda linha."""
 
 
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+sompar = somterc = somseg = 0
+
+for l in range(0, 3):  # Laço para linha
+    for c in range(0, 3): # Laço para coluna
+        matriz[l][c] = int(input(f'Digite um valor [{l}, {c}]: '))
+
+print('-*-' * 30)
+for l in range(0 ,3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+        if matriz[l][c] % 2 == 0:
+            sompar += matriz[l][c]
+    print()
+print('-=-' * 30)
+print(f'A soma dos valores pares é {sompar}')
