18fc3af74b24eba2744cda3f682c05e6d1b08fc4
Naftali Ferreira <naftalibarbosaferreira@gmail.com>
2023-01-27 12:11:12 -0300

    Update Exercício92_.py

--- 3.Dicionários em Python/Exercício92_.py
+++ 3.Dicionários em Python/Exercício92_.py
@@ -1,3 +1,31 @@
 """ Crie um programa que leia o nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionário, 
 se por acaso a CTPS for diferente de 0, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, 
 alem da idade, com quantos anos a pessoa vai se aposentar.  """
+
+# bibliotecas
+from time import sleep
+
+# Dicionário
+
+dados_trabalhador = dict()
+
+# dados auxiliares
+
+aposent = 35  # aposenta com 35 anos de colaboração
+
+# entrada de dados
+print('-=' * 30)
+dados_trabalhador['nome'] = str(input('Nome: '))
+dados_trabalhador['idade'] = int(input('Ano de nascimento: '))
+dados_trabalhador['ctps'] = int(input('Numero da carteira de trabalho: (0 caso não tenha) '))
+if dados_trabalhador['ctps'] != 0:
+    dados_trabalhador['ano contratação'] = int(input('Ano da contratação: '))
+    dados_trabalhador['salário'] = float(input('Salário: '))
+    print('Cadastro concluído!')
+else:
+    print('Cadastro concluído!')
+     
+print('-=' * 30)
+
+# Output
+
