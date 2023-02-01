dd498b1586b0fc3c1b048179b0ab586d6b2e91e1
Naftali Ferreira <naftalibarbosaferreira@gmail.com>
2023-02-01 11:40:49 -0300

    Update Exercício93_CadastrodeJogadordeFutebol.py

--- 3.Dicionários em Python/Exercício93_CadastrodeJogadordeFutebol.py
+++ 3.Dicionários em Python/Exercício93_CadastrodeJogadordeFutebol.py
@@ -5,8 +5,31 @@ tudo isso será guardado em um dicionário, incluindo o total de gols feitos dur
 
 
 
-dados = dict()
+# Listas , dicionários e tuplas
 
-dados['nome'] = str(input('Nome: '))
+jogador = dict()
+partidas = list()
 
-print(dados)
+# Entrada de dados
+
+jogador['nome'] = str(input('Nome do jogador: '))
+total_partidas = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+# Tratamento dos dados
+for c in range(0, total_partidas):
+    partidas.append(int(input(f'    Quantos gols na partida {c+1}? ')))
+jogador['gols'] = partidas[:]
+jogador['total'] = sum(partidas)
+# 1a forma de exibir os resultados
+print('-=' * 30)
+print(jogador)
+print('-=' * 30)
+# 2a forma de exibir os resultados
+for k, v in jogador.items():
+    print(f'{k}: {v}')
+# 3a forma de exibir os resultados
+print('-=' * 30)
+print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas')
+for i, v in enumerate(jogador['gols']):
+    print(f'    - Na partida {i+1}, fez {v} gols.')
+print(f'Foi um total de {jogador["total"]} gols.')
+
