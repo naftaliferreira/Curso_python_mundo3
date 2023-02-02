d962156b0bb737f87f6dcc0110276df909c29bee
Naftali Ferreira <naftalibarbosaferreira@gmail.com>
2023-02-02 00:40:18 -0300

    Update Exercício94_UnindoListaseDicionarios.py

--- 3.Dicionários em Python/Exercício94_UnindoListaseDicionarios.py
+++ 3.Dicionários em Python/Exercício94_UnindoListaseDicionarios.py
@@ -9,3 +9,43 @@ b) A média de idade do grupo
 c) Uma lista com todas as mulheres 
 
 d) Uma lista com todas as pessoas com idade acima da média."""
+
+# Armazenamento de dados
+
+galera = list()
+pessoa = dict()
+tot = 0
+# Input 
+while True:
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
+    tot += 1
+    while True:
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Digite apenas M ou F.')
+    pessoa['idade'] = int(input('Idade: '))
+    galera.append(pessoa.copy())
+    
+    while True:
+        resposta = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resposta in 'SN':
+            break
+        print('Erro! Responda apenas S ou N.')
+    if resposta == 'N':
+        break
+# Tratamento de dados
+print('-=' * 30)
+
+# Quantas pessoas foram cadastradas?
+print(f'Foram cadastradas {tot} pessoas.')
+
+# A média de idade do grupo
+
+
+# Uma lista com todas as mulheres 
+
+
+# Uma lista com todas as pessoas com idade acima da média.
+
