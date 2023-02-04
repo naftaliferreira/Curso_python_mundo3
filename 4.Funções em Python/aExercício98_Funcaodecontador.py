00b676f0ab632d6da3db71889cfab23490dbc490
Naftali Ferreira <naftalibarbosaferreira@gmail.com>
2023-02-04 02:10:14 -0300

    Create 00_aula20.ipynb

---
+++ 4.Funções em Python/aExercício98_Funcaodecontador.py
@@ -0,0 +1,11 @@
+"""Faça um programa que tenha uma função chamada contador(), que receba três parâmetros: Início, Fim e Passo e realize a contagem. 
+
+Seu programa tem que realizar três contagens através da função criada: 
+
+a) De 1 até 10, de 1 em 1
+
+b) De 10 a 0, de 2 em 2
+
+c) Uma contagem personalizada.
+
+"""
---
+++ 4.Funções em Python/aExercício99_FuncaoqueDescobreoMaior.py
@@ -0,0 +1,4 @@
+""" Faça um programa que tenha uma função chamada maior(), que receba vários parâmentros com valores inteiros.
+
+Seu programa tem que analisar todos os valores e dizer qual é o maior. 
+"""
