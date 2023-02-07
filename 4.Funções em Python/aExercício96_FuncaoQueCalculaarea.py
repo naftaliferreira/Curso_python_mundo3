80f7be9c18882975fba68bd2eaf8ba4f346ea1eb
Naftali Ferreira <naftalibarbosaferreira@gmail.com>
2023-02-07 00:10:37 -0300

    adição aula 20 e exercicio 96

--- 4.Funções em Python/aExercício96_FuncaoQueCalculaarea.py
+++ 4.Funções em Python/aExercício96_FuncaoQueCalculaarea.py
@@ -1,2 +1,15 @@
 """ Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno retangular (largura e comprimento) 
 e mostre a área do terreno. """
+
+l = int(input('Qual a largura da área '))
+c = int(input('Qual o comprimento da área? '))
+
+def area(l, c):
+  l2 = l * 2
+  c2 = c * 2
+  resultado = l2 * c2
+
+
+# Programa principal 
+
+print(area)
