a0742b4a4d0fc91b4b37f7f10154ee9c39250a6e
Naftali Ferreira <naftalibarbosaferreira@gmail.com>
2023-02-08 12:38:38 -0300

    Update aExercício99_FuncaoqueDescobreoMaior.py

--- 4.Funções em Python/aExercício99_FuncaoqueDescobreoMaior.py
+++ 4.Funções em Python/aExercício99_FuncaoqueDescobreoMaior.py
@@ -2,15 +2,28 @@
 
 Seu programa tem que analisar todos os valores e dizer qual é o maior. 
 """
+from time import sleep
+
+def maior(* num):
+    cont = maior = 0
+    print('Analisando os números ...')
+    for v in num:
+        print(f'{v}', end=' ', flush=True)
+        sleep(0.3)
+        if cont == 0:
+            maior == 0
+        if cont > 0:
+            maior == num
+    print(f'O maior número é {num}')
 
 
 
+# Programa principal
 
-
-
-
-nums = list()
-
-num = int(input('Digite um número: '))
-nums.append(num[:])
+maior(2, 9, 4, 5, 7, 1)
+# maior(4, 7, 0)
+# maior(1, 2)
+# maior(6)
+# maior()
+ 
 
