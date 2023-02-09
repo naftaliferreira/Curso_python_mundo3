24a3c2869ccd6c44052408127a0c2b0fa02d2846
Naftali Ferreira <naftalibarbosaferreira@gmail.com>
2023-02-09 00:16:52 -0300

    Adição dos exercícios 99 e 100

--- 4.Funções em Python/aExercicio100_FuncoesParaSortearESomar.py
+++ 4.Funções em Python/aExercicio100_FuncoesParaSortearESomar.py
@@ -1,3 +1,33 @@
-""" Faça um programa que tenha uma lista chamada números e duas funções chamadas sorteio() e somaPar(). 
+""" Faça um programa que tenha uma lista chamada números e duas funções chamadas sorteia() e somaPar(). 
 A primeira função vai sortear 5 números e vai colocá-los dentro de uma lista e a segunda função vai
  mostrar a soma de todos os valores pares sorteados pela função anterior.  """
+
+# bibliotecas
+from random import randint
+from time import sleep
+
+# lista
+
+
+ # funções 
+def sorteia(lista):
+    for cont in range(0, 5):
+        n = randint(1, 10)
+        lista.append(n)
+        print(f' {n}', end=' ', flush=True)
+        sleep(0.3)
+    print('fim')
+
+def somaPar(lista):
+    soma = 0
+    for v in lista:
+        if v % 2 == 0:
+            soma += v
+    print(f'somando os valores pares de {lista}, temos {soma}')
+
+ # programas principais
+numeros = list()
+
+sorteia(numeros)
+
+somaPar(numeros)
--- 4.Funções em Python/aExercício99_FuncaoqueDescobreoMaior.py
+++ 4.Funções em Python/aExercício99_FuncaoqueDescobreoMaior.py
@@ -6,24 +6,27 @@ from time import sleep
 
 def maior(* num):
     cont = maior = 0
-    print('Analisando os números ...')
+    print('-=' * 20)
+    print('Analisando os números passados ...')
     for v in num:
         print(f'{v}', end=' ', flush=True)
         sleep(0.3)
         if cont == 0:
-            maior == 0
-        if cont > 0:
-            maior == num
-    print(f'O maior número é {num}')
-
+            maior = v
+        else:
+            if v > maior:
+                maior = v
+        cont += 1
+    print(f'Foram informados {cont} valores ao todo.')
+    print(f'O maior valor informado foi {maior}.')
 
 
 # Programa principal
 
 maior(2, 9, 4, 5, 7, 1)
-# maior(4, 7, 0)
-# maior(1, 2)
-# maior(6)
-# maior()
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior()
  
 
