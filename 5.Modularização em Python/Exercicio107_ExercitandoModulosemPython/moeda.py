bd9033b69a1128cc8bf5957b7b825653b968da5b
Naftali Ferreira <naftalibarbosaferreira@gmail.com>
2023-02-16 00:20:31 -0300

    Adição do exercício 107

--- 5.Modularização em Python/Exercicio107_ExercitandoModulosemPython/moeda.py
+++ 5.Modularização em Python/Exercicio107_ExercitandoModulosemPython/moeda.py
@@ -1,32 +1,37 @@
 
 # Funções moeda
 
-#  => função dobro
-
-def dobro(n):
-    dobro = n*2
-    print(f'O dobro de {n} é ${dobro}') 
-    return
-
-#  => função metade
-
-def metade(n):
-    metade = n/2
-    print(f'A metade de {n} é ${metade}')
-
-
 # => Aumento 
 
-def aumento(n):
-    percent = int(input('Percentual do aumento: '))
-    aumento = ((n * percent) / 100) + n
-    print(f'Aumentando {percent}% é  ${aumento}')
+def aumentar(preco, taxa):
+    result =  preco + (preco * taxa / 100)
+    return result
 
 
 # => Diminuindo
 
-def diminuindo(n):
-    percent = int(input('Percentual do desconto: '))
-    dimin = n - ((n * percent) / 100)
-    print(f'Diminuindo {percent}% é ${dimin}')
+def diminuir(preco, taxa):
+    result = preco - (preco * taxa / 100)
+    return result
+
+
+#  => função dobro
+
+def dobro(preco):
+   result = preco * 2
+   return result
+
+
+#  => função metade
+
+def metade(preco):
+   result = preco / 2
+   return result
+
+
+
+
+
+    
+
 
