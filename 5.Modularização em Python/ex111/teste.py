b0868d6333c98493a0561b304b51bb51e99939d0
Naftali Ferreira <naftalibarbosaferreira@gmail.com>
2023-03-04 11:22:28 -0300

    Atualização ex111 e 112

---
+++ 5.Modularização em Python/ex111/teste.py
@@ -0,0 +1,5 @@
+from CursoemVideo.ex111.utilidadescev import moeda
+
+p = float(input('Digite um preço: R$'))
+moeda.resumo(p, 34.5, 16.8)
+
---
+++ 5.Modularização em Python/ex112/utilidadescev/dado/__init__.py
@@ -0,0 +1,13 @@
+
+# função de validação de dados
+
+def leiaDinheiro(msg):
+    valido = False
+    while not valido:
+        entrada = str(input(msg)).replace(',', '.').strip()
+        if entrada.isalpha() or entrada == '':
+            print(f'\033[0;31mERROR!\"{entrada}" é um preço inválido!\033[m')
+        else:
+            valido = True
+            return float(entrada)
+
